5fb495d4bdcbe26aa8970319fe5f7960045ded93
Patrick Henegan <phenegan03@gmail.com>
2020-02-14 14:32:18 -0500

    More Array changes
    added some more example methods for arrays with explanations

--- src/Arrays.java
+++ src/Arrays.java
@@ -11,14 +11,14 @@ public class Arrays {
         //There are 2 ways to define arrays. The first is by setting the length and leaving the values empty
         array = new int[5]; //This creates an array with 5 empty ints (if you don't set them they will be 0)
 
-        //                       0      1     2     3      4
+        //Position in the array:  0     1      2     3     4
         array2 = new boolean[] {true, false, true, true, false};
 
-        /**
-         * This is not the proper use for Documentation, but it stands out.
-         * Note that you can't change the length of an array after you make it, unless you wipe all of the values by setting it
-         * as a new array
-         */
+        //Note that you can't change the length of an array after you make it, unless you wipe all of the values by setting it
+        //as a new array
+
+        //array2 = new boolean[6]; //This will erase all of the values in the previously defined array, and won't just add an extra spot
+
 
         //This creates an array with a defined list of values
         //Because computers start counting at 0, it is important to remember that the last value of an array is actually the length minus 1.
@@ -31,38 +31,62 @@ public class Arrays {
         array[4] = 9990;
         System.out.println(array[0]);
 
-        System.out.println("Array 1: ");
+        System.out.print("Array 1: ");
         //Arrays are useful in methods when you want to take in a set of values,
         //but don't know the exact amount of values you want to take in.
-        //Read the printNumbers() and
+        //Read the printNumbers() and printBooleans() methods to see ways to print an entire array
         printNumbers(array);
 
+        //Prints a new line before moving onto the next arrays
+        System.out.println();
+        System.out.print("Array 2: ");
+        printBooleans(array2);
 
-        //There is no way to print or set values of the entire array, so a convenient way to do that is with loops
+        //See the setValues() and testInts() methods to see other ways arrays can be used in methods
+        printNumbers(setValues(array));
+
+        System.out.println();
+        printBooleans(testInts(array));
 
     }
 
     public static void printNumbers(int[] nums) {
+        //There is no way to print or set values of the entire array, so a convenient way to do that is with loops
         //you can get the length of the array by doing name.length, which is useful for setting up for() loops
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i] + " ");
+            System.out.print(nums[i] + " ");
         }
     }
 
     public static void printBooleans(boolean[] bools) {
         //This is a special type of for loop that works for lists and ArrayLists (this will be discussed later)
+        //Can be read as - for each boolean (name) in (array name)
         for (boolean bool : bools) {
             //Note that this type of loop will not work when you want to change one of the values in the array
             //bool = true won't change the actual value in the array because of this
-            System.out.println(bool);
+            System.out.print(bool + " ");
         }
+        System.out.println();
     }
 
-    public int[] setValues(int[] list) {
+    public static int[] setValues(int[] list) {
+        //Example of a method that goes through an array and outputs a new array based on the values of the first one
+        int[] newList = new int[list.length];
+
         for (int i = 0; i < list.length; i++) {
-            list[i] = i * 4;
+            newList[i] = (i + 1) * 4 + list[i];
         }
         return list;
+    }
+
+    public static boolean[] testInts(int[] list) {
+        //This is an example of a method that might take in one type of an array and use the data to create an array with a different type
+        boolean[] solutions = new boolean[list.length];
+        for (int i = 0; i < list.length; i++) {
+            //Sets the value in solutions[] based on whether or not a corresponding value in list[] is greater than 5
+            solutions[i] = (list[i] > 50);
+        }
+        return solutions;
     }
 
 
